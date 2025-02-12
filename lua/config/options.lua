f4364d42bb8e060b9b31fa4381cf16415093813d
Yuvraj Cheema <yuvrajscheema@gmail.com>
2025-02-12 15:32:31 -0500

    added lots of remaps and options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,10 +2,27 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
 opt.tabstop = 2
+opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+
+opt.updatetime = 50
+
+opt.termguicolors = true
+opt.colorcolumn = "80"
+
 opt.ignorecase = true
 opt.smartcase = true
 opt.splitright = true
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,1 +1,24 @@
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+local set = vim.keymap.set
+
+set("n", "<leader>pv", ":Ex<CR>")
+
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '>-2<CR>gv=gv")
+
+set("n", "J", "mzJ`z")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
+set("n", "n", "nzzzv")
+set("n", "N", "Nzzzv")
+
+set("x", "<leader>p", "\"_dP")
+
+set("n", "<leader>y", "\"+y")
+set("v", "<leader>y", "\"+y")
+set("n", "<leader>Y", "\"+Y")
+
+set("n", "leader>d", "\"_d")
+set("v", "leader>d", "\"_d")
+
+set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
