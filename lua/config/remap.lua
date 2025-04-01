47126a231298ee8d7bea39dacdacb593a4eda330
Yuvraj Cheema <yuvrajscheema@gmail.com>
2025-04-01 13:02:11 -0400

    added shortcut for turning off lsp

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -24,3 +24,5 @@ set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
 
 set("n", "<leader>u", ":UndotreeToggle<CR>")
+
+set("n", "<leader>ls", ":LspStop<CR>")
